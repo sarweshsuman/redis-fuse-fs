f74f16676a25b754c71d68db1fd2baedb06e09b9
Sarwesh Suman <sarwesh.suman@walmartlabs.com>
2020-05-17 16:10:34 +0530

    added test code

--- fs-impl.go
+++ fs-impl.go
@@ -8,42 +8,42 @@ import (
 )
 
 func (fs *RedisFuseConfig) GetAttr(name string, ctx *fuse.Context) (*fuse.Attr, fuse.Status) {
-	fmt.Print("Request to GetAttr of {}\n", name)
+	fmt.Print("Request to GetAttr of %v\n", name)
 	return nil, fuse.ENOENT
 }
 
 func (fs *RedisFuseConfig) OpenDir(name string, ctx *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
-	fmt.Print("Request to Open Dir {}\n", name)
+	fmt.Print("Request to Open Dir %v\n", name)
 	return []fuse.DirEntry{}, fuse.OK
 }
 
 func (fs *RedisFuseConfig) Open(name string, flags uint32, ctx *fuse.Context) (nodefs.File, fuse.Status) {
-	fmt.Print("Request to Open {}\n", name)
+	fmt.Print("Request to Open %v\n", name)
 	return nil, fuse.ENOENT
 }
 
 func (fs *RedisFuseConfig) Create(name string, flags uint32, mode uint32, ctx *fuse.Context) (nodefs.File, fuse.Status) {
-	fmt.Print("Request to Create {}\n", name)
+	fmt.Print("Request to Create %v\n", name)
 	return nil, fuse.ENOENT
 }
 
 func (fs *RedisFuseConfig) Rename(oldName string, newName string, ctx *fuse.Context) fuse.Status {
-	fmt.Print("Request to Rename {}\n", oldName)
+	fmt.Print("Request to Rename %v\n", oldName)
 	return fuse.ENOENT
 }
 
 func (fs *RedisFuseConfig) Unlink(name string, ctx *fuse.Context) fuse.Status {
-	fmt.Print("Request to Unlink {}\n", name)
+	fmt.Print("Request to Unlink %v\n", name)
 	return fuse.ENOENT
 }
 
 func (fs *RedisFuseConfig) Rmdir(name string, ctx *fuse.Context) fuse.Status {
-	fmt.Print("Request to Rmdir {}\n", name)
+	fmt.Print("Request to Rmdir %v\n", name)
 	return fuse.ENOENT
 }
 
 func (fs *RedisFuseConfig) Mkdir(name string, mode uint32, ctx *fuse.Context) fuse.Status {
-	fmt.Print("Request to Mkdir {}\n", name)
+	fmt.Print("Request to Mkdir %v\n", name)
 	return fuse.OK
 }
 
